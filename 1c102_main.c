30ca208544c69114c7520a721b72c0a6ecebbe6d
337273158 <337273158@qq.com>
2024-07-12 11:23:32 +0800

    Add files via upload
    8266main

--- 1c102_main.c
+++ 1c102_main.c
@@ -10,19 +10,6 @@
  *
  * 实验现象：通过ESP8266上传温湿度数据至云平台
 ******************************************************/
-// #include "ls1x.h"
-// #include "Config.h"
-// #include "ls1x_gpio.h"
-// #include "ls1x_latimer.h"
-// #include "esp8266.h"
-// #include "ls1c102_interrupt.h"
-// #include "iic.h"
-// #include "oled.h"
-// #include "dht11.h"
-// #include "BEEP.h"
-// #include "key.h"
-// #include "led.h"
-
 #include "ls1x.h"
 #include "Config.h"
 #include "ls1x_gpio.h"
@@ -38,189 +25,124 @@
 #include "led.h"
 #include "queue.h"
 #include "ls1x_clock.h"
+#include "esp8266.h"
+
 #define LED 20
 
 char str[50];
-static uint16_t temperature;
-static uint16_t humidity;
-uint8_t cnt;
+static uint16_t temp;
+static uint16_t humi;
+static uint16_t ptemp;
+static uint8_t shock;
+uint8_t data[8] = {0x55, 0xAA, 0x00, 0x00, 0x00, 0x00, 0x01, 0xBB}; //温湿度数据上云平台    数据包
+
 uint8_t received_data = 0;
-uint8_t data[3];
+
 uint8_t Read_Buffer[255]; // 设置接收缓冲数组
 uint8_t Read_length;
+uint8_t da = 0;
 int main(int arg, char *args[])
 {
     SystemClockInit(); // 时钟等系统配置
     GPIOInit();        // io配置
     OLED_Init();
-    Init_shock();
-    EnableInt(); // 开总中断
-    // Uart1_init(9600);
-    DL_LN3X_Init(DL_LN3X_NODE,CHANNEL,Network1_Id);//设置为主机（接收端），设置信道为0x12，网络地址为0x0003
-    Queue_Init(&Circular_queue);
+    Uart1_init(9600);
     BEEP_Init();
-    DHT11_Init();                               // DHT11初始化  
-    OLED_Show_Str(40, 0, "人员 1", 16); // OLED显示界面
-    OLED_Show_Str(20, 3, "体温:     ℃", 16);     // OLED显示界面
-    OLED_Show_Str(20 ,6, "行动: 正常", 16);
-    data[2] = 0x02;
+    EnableInt(); // 开总中断
+    DL_LN3X_Init(DL_LN3X_NODE, CHANNEL, Network1_Id);
+    Queue_Init(&Circular_queue);
+    // sprintf(str, "体度:    ℃", ptemp / 10);
+    // OLED_Show_Str(0, 0, "智能冷库安全第一", 16);    // OLED显示界面
+    // sprintf(str, "行动:   ", shock);
+    OLED_Show_Str(5, 1, "冷库东空调温度：  ", 16);    // OLED显示界面
+    // // sprintf(str, "温度:    ℃", temp / 10);
+    // OLED_Show_Str(5, 4, "当前温度:   ℃", 16);    // OLED显示界面
+    // // sprintf(str, "湿度:    %%RH", humi / 10);
+    // OLED_Show_Str(5, 6, "设定温度:   ℃", 16);    // OLED显示界面
+    // OLED_Show_Str(10, 0, "ZigBee通信实验", 16); // OLED显示界面
     while (1)
     {
-        DHT11_Read_Data(&temperature, &humidity);
-        data[0] = 0x02 ;
-        data[1] = temperature;
-        
-        if(shock_get()==1)
+    //     delay_ms(1000);
+    //    BEEP_ON;
+    //    delay_ms(1000);
+        data[2] = ptemp ;
+        data[3] = shock;
+        data[4] = temp ;
+        data[5] = humi ;
+        // sprintf(str, "%2d", ptemp );
+        // OLED_Show_Str(52, 0, str, 16);    // OLED显示界面
+        // sprintf(str, "%d", shock);
+        // OLED_Show_Str(52, 2, str, 16);    // OLED显示界面
+        // sprintf(str, "%2d", temp );
+        // OLED_Show_Str(80, 4, str, 16);    // OLED显示界面
+        sprintf(str, "%2d", 50-temp );
+        OLED_Clear;
+        OLED_Show_Str(58, 3, str, 16);    // OLED显示界面
+
+
+        if (Queue_isEmpty(&Circular_queue) == 0) // 判断队列是否为空，即判断是否收到数据
         {
-        OLED_Show_Str(70, 6, "正常", 16);           //显示state
-        data[2] = 0x02;
-        BEEP_OFF;
+            Read_length = Queue_HadUse(&Circular_queue);           // 返回队列中数据的长度
+            Queue_Read(&Circular_queue, Read_Buffer, Read_length); // 读取队列缓冲区的值到接收缓冲区
+            if(Read_Buffer[6]==0x02)           //
+            {
+                ptemp = Read_Buffer[7] ;
+                shock = Read_Buffer[8] ;
+
+                // BEEP_ON;
+            }
+            if(Read_Buffer[6]==0x03)
+            {
+                temp = Read_Buffer[7] ;
+                humi = Read_Buffer[9] ;
+                // temp = Read_Buffer[7] << 8 | Read_Buffer[8];
+                // humi = Read_Buffer[9] << 8 | Read_Buffer[10];
+                // BEEP_ON;
+            }
         }
         else
         {
-        // OLED_Show_Str(70, 6, "异常", 16); 
-        cnt++;
-        if(cnt==15)
-            {
-            // data[3] = 0;
-            BEEP_ON;
-            OLED_Show_Str(70, 6, "异常", 16);
-            }
-        if(cnt==25)
-            {
-            // BEEP_OFF;
-            data[2] = 0x01;
-            OLED_Show_Str(70, 6, "异常", 16);
-            }
-
+            memset(Read_Buffer, 0, 255); // 填充接收缓冲区为0p
         }
-        // data[2] = temperature % 256;
-        // data[3] = shock_get();
-        // data[2] = humidity / 256;
-        // data[3] = humidity % 256;
-        sprintf(str, "%2d", temperature);
-        OLED_Show_Str(70, 3, str, 16);           //显示WEN度
+        // BEEP_OFF;
+
         
-        // if(shock_get()==1)
-        // {
-        // OLED_Show_Str(70, 6, "正常", 16);           //显示state
-        // // BEEP_OFF;
-        // }
-        // else
-        // // {
-        // // // OLED_Show_Str(70, 6, "异常", 16); 
-        // // cnt++;
-        // // if(cnt==20)
-        //     {
-        //     // BEEP_ON;
-        //     OLED_Show_Str(70, 6, "异常", 16);
-        //     }
-        // // }
-        // sprintf(str, "温度: %2d ℃", temperature / 10);
-        // OLED_Show_Str(5, 4, str, 16);    // OLED显示界面
-        // sprintf(str, "湿度: %2d %%RH", humidity / 10);
-        // OLED_Show_Str(5, 6, str, 16);    // OLED显示界面
-        delay_ms(200);
-        DL_LN3X_Send(data, 3, ZIGBEE_RX_NODE);
+        if(wifi_connected == 0)
+        {
+            if(esp8266_check_cmd('T'))     //当收到'T'字符时，表示ESP8266连接成功
+            {
+                // OLED_Clear(); // OLED显示界面
+                wifi_connected = 1;
+                // OLED_Show_Str(12, 3, "已连接到WIFI", 16);
+                // delay_ms(1000);
+                // OLED_Clear(); // OLED显示界面
+                gpio_write_pin(LED, 1);
+                BEEP_ON;
+                delay_ms(500);
+                BEEP_OFF;
+            }
+        }
+        if(wifi_connected == 1)
+        {
+            delay_ms(2000);
+        // data[2] = ptemp / 10;
+        // data[3] = shock;
+        // data[4] = temp / 10;
+        // data[5] = humi / 10;
+        // sprintf(str, "%2d", ptemp / 10);
+        // OLED_Show_Str(52, 0, str, 16);    // OLED显示界面
+        // sprintf(str, "%d", shock);
+        // OLED_Show_Str(52, 2, str, 16);    // OLED显示界面
+        // sprintf(str, "%2d", temp / 10);
+        // OLED_Show_Str(52, 4, str, 16);    // OLED显示界面
+        // sprintf(str, "%2d", humi / 10);
+        // OLED_Show_Str(52, 6, str, 16);    // OLED显示界面
+            // data[6] = (data[2] + data[3] + data[4] + data[5]) % 256;   //计算校验和
+            printf("%s",data);
+            UART_SendDataALL(UART1, data, 8);
+            
+        }
     }
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-// #define LED 20
-
-// char str[50];
-// static uint16_t temperature;
-// static uint16_t humidity;
-
-// uint8_t received_data = 0;
-// uint8_t data[6];
-// uint8_t Read_Buffer[255]; // 设置接收缓冲数组
-// uint8_t Read_length;
-// // char shock[7];
-// uint8_t data[8] = {0x55, 0xAA, 0x00, 0x00, 0x00, 0x00, 0x00, 0xBB}; //温湿度数据上云平台    数据包
-// uint8_t cnt = 0;
-// int main(int arg, char *args[])
-// {
-//     SystemClockInit(); // 时钟等系统配置
-//     GPIOInit();        // io配置
-//     OLED_Init();
-//     EnableInt(); // 开总中断
-//     Init_shock();
-//     // Queue_Init(&Circular_queue);
-//     BEEP_Init();
-//     Uart0_init(9600); // 串口0初始化，io06 io07   串口初始化需要在开启EnableInt之后
-
-//     // OLED_Show_Str(10, 0, "温湿度监测实验", 16); // OLED显示界面
-
-//     while (DHT11_Init()) // 检测是否接入温湿度传感器
-//     {
-//         OLED_Show_Str(10, 4, "未检测到传感器", 16); // OLED显示界面
-//     }
-//     OLED_Show_Str(0, 0, "人员健康监测系统", 16); // OLED显示界面
-//     OLED_Show_Str(20, 3, "体温:     ℃", 16);     // OLED显示界面
-//     OLED_Show_Str(20 ,6, "行动: 正常", 16);
-//     while (1)
-//     {
-//         DHT11_Read_Data(&temperature, &humidity); // 读取温湿度值state
-//         // char shock[] = shock_get();
-//         data[2] = temperature / 10;     //将温湿度数据存放至数据包中
-//         data[3] = shock_get();
-//         // data[3] = humidity / 10;
-
-//         // sprintf(str, "%2d", temperature / 10);     
-//         // OLED_Show_Str(70, 0, str, 16);           
-//         sprintf(str, "%2d", temperature / 10);
-//         OLED_Show_Str(70, 3, str, 16);           //显示WEN度
-//         // sprintf(str, "%d", shock_get());
-//         if(shock_get()==1)
-//         {
-//         OLED_Show_Str(70, 6, "正常", 16);           //显示state
-//         BEEP_OFF;
-//         // delay_ms(1500);
-//         }
-//         else{OLED_Show_Str(70, 6, "异常", 16); 
-//         cnt++;
-
-//         if(cnt==10)BEEP_ON;
-        
-//         }
-//         if(wifi_connected == 0)
-//         {
-//             if(esp8266_check_cmd('T'))  //当收到'T'字符时，表示ESP8266连接成功
-//             {
-//                 OLED_Clear(); // OLED显示界面
-//                 wifi_connected = 1;
-//                 OLED_Show_Str(12, 3, "已连接到WIFI", 16);
-//                 delay_ms(1000);
-//                 OLED_Clear(); // OLED显示界面
-//                 delay_ms(500);
-//                 OLED_Show_Str(0, 0, "人员健康监测系统", 16); // OLED显示界面
-//                 OLED_Show_Str(20, 3, "体温:    ℃", 16);     // OLED显示界面
-//                 OLED_Show_Str(20, 6, "行动:   ", 16);
-//                 gpio_write_pin(LED, 1);
-//                 BEEP_ON;
-//                 delay_ms(500);
-//                 BEEP_OFF;
-//             }
-//         }
-//         if(wifi_connected == 1)
-//         {
-//             delay_ms(20);
-//             data[6] = (data[2] + data[3] + data[4] + data[5]) % 256;   //计算校验和
-//             printf("%s",data);
-//             UART_SendDataALL(UART0, data, 8);
-            
-//         }
-//     }
-
-//     return 0;
-// }
